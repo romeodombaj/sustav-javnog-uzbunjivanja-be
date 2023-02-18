219b04e48264c7127bab2c2c9dc3a2ded6fbbefa
romeodombaj <romeodombaj@gmail.com>
2023-02-18 14:34:05 +0100

    initial commit

---
+++ src/main/java/romeodombaj/sustavjavnoguzbunjivanjabe/SustavJavnogUzbunjivanjaBeApplication.java
@@ -0,0 +1,13 @@
+package romeodombaj.sustavjavnoguzbunjivanjabe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SustavJavnogUzbunjivanjaBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SustavJavnogUzbunjivanjaBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/romeodombaj/sustavjavnoguzbunjivanjabe/SustavJavnogUzbunjivanjaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package romeodombaj.sustavjavnoguzbunjivanjabe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SustavJavnogUzbunjivanjaBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
